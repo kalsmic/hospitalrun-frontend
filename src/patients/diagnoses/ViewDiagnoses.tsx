896d376b32127713d62838d0e15a3eb15323b38e
Arthur Kalule <kalulearthur@gmail.com>
2023-09-08 18:34:24 +0000

    add types

--- src/patients/diagnoses/ViewDiagnoses.tsx
+++ src/patients/diagnoses/ViewDiagnoses.tsx
@@ -4,7 +4,7 @@ import { useParams } from 'react-router-dom'
 import DiagnosisTable from './DiagnosisTable'
 
 const ViewDiagnoses = () => {
-  const { id: patientId } = useParams()
+  const { id: patientId } = useParams<{id:any}>()
 
   return <DiagnosisTable patientId={patientId} />
 }
--- src/patients/view/ViewPatient.tsx
+++ src/patients/view/ViewPatient.tsx
@@ -39,7 +39,7 @@ const ViewPatient = () => {
   const { path } = useRouteMatch()
   const setButtonToolBar = useButtonToolbarSetter()
 
-  const { id } = useParams()
+  const { id } = useParams<{id:any}>()
   const { permissions } = useSelector((state: RootState) => state.user)
   const { data: patient, status } = usePatient(id)
 
